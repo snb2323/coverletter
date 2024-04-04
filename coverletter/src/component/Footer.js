ab3f652a14b05b6ba73a2ff81ef00250f9c8a44c
snb2323 <dheltus1r@naver.com>
2024-04-05 01:33:21 +0900

    footer 수정

--- coverletter/src/component/Footer.js
+++ coverletter/src/component/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
 
                     <a href="https://github.com/snb2323/CRA-coverletter.git" target="blank"> <FaGithub className=" mb-3"></FaGithub></a>
 
-                    <a href="https://www.figma.com/proto/3ePwKovJqYTbLRALUgC49p/%EA%B0%9C%EC%9D%B8%ED%8F%AC%ED%8F%B4?type=design&node-id=511-19&t=jFKbgYuW4pDZIyMi-0&scaling=min-zoom&page-id=0%3A1" target="blank">   <FaFigma className=" mb-3"></FaFigma></a>
+                    <a href="https://www.figma.com/file/RDBdOswXvMWdwM5ZCbxrqF/%EC%9D%B4%EB%8F%99%EB%AF%BC-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EC%9E%90%EC%86%8C%EC%84%9C?type=design&node-id=0%3A1&mode=dev&t=EBxdpgLPA3ZjixvA-1" target="blank">   <FaFigma className=" mb-3"></FaFigma></a>
 
                     <a href="https://even-amber-f05.notion.site/1e7173072d194f90a7c2f3a8bb04b2ce"> <img src="./img/notion.svg" className="notionimg"></img></a>
                 </div>
